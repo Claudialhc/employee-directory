aea23c41a379eda6714cf0ff48cc578e8de130e7
Claudia Hernandez <claudiahdez13@gmail.com>
2021-02-16 19:14:10 -0800

    created API file and added the random user generator ajax code to file

---
+++ src/API.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+// const BASEURL = "https://www.omdbapi.com/?t=";
+// const APIKEY = "&apikey=trilogy";
+
+export default {
+    $.ajax({
+        url: 'https://randomuser.me/api/',
+        dataType: 'json',
+        success: function(data) {
+          console.log(data);
+        }
+      })
+            
+      
+  };
